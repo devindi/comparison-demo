52eed3007b2371b6ba8125b2347dbc8bfe6a5529
Andrew <andrew@devindi.com>
2018-02-11 02:27:35 +0300

    Implemented simple performance test

--- app/src/main/java/com/devindi/mapper/demo/BenchmarkActivity.java
+++ app/src/main/java/com/devindi/mapper/demo/BenchmarkActivity.java
@@ -11,7 +11,6 @@ import com.devindi.mapper.demo.model.complex.Order;
 import com.devindi.mapper.demo.model.simple.Person;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class BenchmarkActivity extends Activity {
@@ -21,22 +20,23 @@ public class BenchmarkActivity extends Activity {
         super.onCreate(savedInstanceState);
 
         View view = new View(this);
+        view.setId(R.id.root);
 
         view.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                Provider provider = new Provider();
-//                List<Order> orders = new ArrayList<>();
-//                List<OrderDto> dtos = new ArrayList<>();
+                Provider provider = new Provider();
+                List<Order> orders = new ArrayList<>();
+                List<OrderDto> dtos = new ArrayList<>();
                 Mapper mapper = new Mapper();
-//                for (int i = 0; i < 10; i++) {
-//                    orders.add(provider.getRandomOrder());
-//                }
-//                Log.d("mapping", "onClick: start mapping");
-//                for (int i = 0; i < 500_000; i ++) {
-//                    mapper.toPersonDto(new Person("a" + i, null));
-//                }
-//                Log.d("mapping", "onClick: finish mapping");
+                for (int i = 0; i < 10; i++) {
+                    orders.add(provider.getRandomOrder());
+                }
+                Log.d("mapping", "onClick: start mapping");
+                for (int i = 0; i < 500_000; i ++) {
+                    mapper.toPersonDto(new Person("a" + i, null));
+                }
+                Log.d("mapping", "onClick: finish mapping");
             }
         });
         setContentView(view);
